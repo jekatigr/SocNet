a350012d89aea6afd2ff420d7d0fb6ff41ba5ebe
jekatigr <jekatigr@yandex.ru>
2015-04-07 13:06:58 +0300

    Обновлена БД.

--- socnet.sql
+++ socnet.sql
@@ -18,6 +18,32 @@ USE `socnet`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(45) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `poster_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `profiles`
 --
 
@@ -45,7 +71,7 @@ CREATE TABLE `profiles` (
 
 LOCK TABLES `profiles` WRITE;
 /*!40000 ALTER TABLE `profiles` DISABLE KEYS */;
-INSERT INTO `profiles` VALUES (1,'Me','User','1.png',0,'1993-09-22','Russia','Moscow','ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano '),(2,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL),(3,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL),(4,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL),(5,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL),(6,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL),(7,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL),(8,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL),(9,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL),(10,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL);
+INSERT INTO `profiles` VALUES (1,'Naik','Borzov','1.png',0,'1993-09-22','Russia','Moscow','ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano ano '),(2,'Roger','Oliver','',0,'2014-04-14','China','Heshan','donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium'),(3,'Fred','Wheeler','',0,'2014-11-17','Sweden','Järfälla','neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit'),(4,'Gloria','Oliver','',0,'2014-04-07','Finland','Muurla','volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu'),(5,'Jessica','Rivera','',1,'2015-01-28','France','Paris La Défense','cras in purus eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque aenean auctor gravida sem praesent id massa id'),(6,'Melissa','Price','',1,'2014-04-19','China','Annan','lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta volutpat quam pede lobortis ligula sit amet eleifend pede libero quis'),(7,'Daniel','Fisher','',0,'2014-05-24','Ukraine','Trostyanets’','blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci eget'),(8,'Ann','Owens','',1,'2014-09-11','Russia','Khosta','viverra diam vitae quam suspendisse potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede venenatis'),(9,'Mildred','Ellis','',0,'2015-02-11','Spain','Sevilla','justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis'),(10,'Eric','Willis','',1,'2014-11-23','China','Anle','consequat nulla nisl nunc nisl duis bibendum felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu massa donec dapibus duis at velit eu est congue elementum in hac habitasse platea dictumst morbi vestibulum velit id pretium iaculis diam erat fermentum justo nec condimentum');
 /*!40000 ALTER TABLE `profiles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,32 +103,6 @@ INSERT INTO `users` VALUES (1,'jekatigr','0abf0076ec5f741db3189885a9acca6b'),(2,
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
---
--- Table structure for table `posts`
---
-
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `text` varchar(300) DEFAULT NULL,
-  `date` timestamp NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `posts`
---
-
-LOCK TABLES `posts` WRITE;
-set @@session.time_zone='+00:00';
-SET SESSION sql_mode='ALLOW_INVALID_DATES';
-INSERT INTO `posts` VALUES (1,1,'1 Post', '2009-06-04 22:00:00'),(2,1,'2nd Post', '2014-12-31 00:00:00');
-UNLOCK TABLES;
-
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -111,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-28  2:25:33
+-- Dump completed on 2015-04-07 13:06:29
